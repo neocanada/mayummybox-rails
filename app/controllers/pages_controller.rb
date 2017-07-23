6b51e66c52b83c06ebd6e3684f0cd783d3458b09
sebastien le moan <slemoan@gmail.com>
2017-07-23 22:36:56 +0200

    add pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def concept
+  end
+
+  def parrainage
+  end
+
+  def press
+  end
+
+  def faq
+  end
+
+  def cgu
+  end
+
+  def mentions
+  end
+
+  def affiliation
+  end
+end
